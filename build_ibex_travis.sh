755e47c1c329bef77fa0cbe68f9cbaf4818efca8
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2016-08-25 21:45:42 +0200

    [src] Update travis scripts

---
+++ build_ibex_travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+# check to see if ibex folder is empty
+if [ ! -e "$HOME/ibex/lib/libibex.a" ]; then
+	git clone https://github.com/ibex-team/ibex-lib.git
+	cd ibex-lib
+	./waf configure
+	./waf install
+	make install -j4
+else
+  echo 'Using cached directory.';
+fi
+
+# check to see if ibex-geometry folder is empty
+if [ ! -e "$HOME/ibex-geometry/LICENSE" ]; then
+	git clone https://github.com/benEnsta/ibex-geometry.git
+	cd ibex-geometry
+	cmake .
+	make install -j4
+else
+  echo 'Using cached directory.';
+fi
+
+
